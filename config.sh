298f8d76977143aff67f474b1425a02ca232c671
Kelly Roberts <105584129+bubstance@users.noreply.github.com>
2022-06-30 20:08:49 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -12,5 +12,3 @@ QEMU_IMG_MOUNT_DIR="./mnt/"
 # Note on the offset: https://www.cloudsavvyit.com/7517/how-to-mount-a-qemu-virtual-disk-image/
 QEMU_IMG_MOUNT_OFFSET=32256
 TEMPLEOS_ISO="./TempleOS.ISO"
-TINKEROS_ISO="./TinkerOS.ISO"
-SHRINE_ISO="./Shrine.ISO"
